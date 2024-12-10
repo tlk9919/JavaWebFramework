30bb78981a4d4a76f646b8ec0fc9233f7685dcb9
tlk9919 <tlk_sure@126.com>
2024-12-10 19:08:47 +0800

    数据库配置和手动生成setter和getter“

--- lession17-springboot-interceptator/src/main/java/com/example/lession17springbootinterceptator/model/User.java
+++ lession17-springboot-interceptator/src/main/java/com/example/lession17springbootinterceptator/model/User.java
@@ -19,7 +19,7 @@ import lombok.Setter;
  * @author Your Name
  * @since 2024-12-10
  */
-@Data
+
 @TableName("user")
 public class User implements Serializable {
 
@@ -39,4 +39,55 @@ public class User implements Serializable {
 
     @TableField("updated_at")
     private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
 }
